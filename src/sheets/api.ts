e7aa0bf7849c62f200d0c243a743cb975ebc36a6
horita-yuya <horitayuya@gmail.com>
2024-10-17 23:31:07 +0900

    Update

--- src/sheets/api.ts
+++ src/sheets/api.ts
@@ -4,12 +4,42 @@ import type { SheetRange } from "./types";
 
 const url = "https://sheets.googleapis.com/v4/spreadsheets";
 
+export async function getSpreadsheet(
+  {
+    spreadsheetId,
+    sheet,
+    range,
+    includeGridData = false,
+  }: {
+    spreadsheetId: string;
+    sheet: string;
+    range: SheetRange;
+    includeGridData?: boolean;
+  },
+  init?: RequestInit,
+): Promise<{
+  spreadsheetId: string;
+  sheets: string[];
+}> {
+  const ranges = `${sheet}!${range}`;
+  const search = new URLSearchParams({
+    ranges,
+    includeGridData: `${includeGridData}`,
+  }).toString();
+  const response = await fetch(`${url}/${spreadsheetId}?${search}`, init);
+  return await response.json();
+}
+
 export async function getSpreadsheetValues(
   {
     spreadsheetId,
     sheet,
     range,
-  }: { spreadsheetId: string; sheet: string; range: SheetRange },
+  }: {
+    spreadsheetId: string;
+    sheet: string;
+    range: SheetRange;
+  },
   init?: RequestInit,
 ): Promise<{ range: string; values: string[][] }> {
   const value = `${sheet}!${range}`;
